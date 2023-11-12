0ffbe470822a3baaa6f3641101109b98107e9620
Matt Styles <matt@personalurban.com>
2023-11-12 20:09:45 +0000

    add :rocket: vanilla extract

--- apps/playground/next.config.js
+++ apps/playground/next.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+import {createVanillaExtractPlugin} from '@vanilla-extract/next-plugin'
+const withVanillaExtract = createVanillaExtractPlugin()
+
+const config = {
   reactStrictMode: true,
   transpilePackages: ['ui'],
 }
+
+module.exports = withVanillaExtract(config)
